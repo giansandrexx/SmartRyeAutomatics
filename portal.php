885956178117b3ffb2001dff09ba57dbec13c201
Sand Rex <137732075+giansandrexx@users.noreply.github.com>
2026-03-02 10:52:55 +0800

    Update portal.php

--- portal.php
+++ portal.php
@@ -26,8 +26,8 @@ if (isset($_GET['logout'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="sratool/css/portal.css">
-    <link rel="stylesheet" href="/smartryesystem/sratool/css/responsive.css">
+    <link rel="stylesheet" href="/sratool/css/portal.css">
+    <link rel="stylesheet" href="/sratool/css/responsive.css">
 </head>
 <body>
 
@@ -139,4 +139,5 @@ if (isset($_GET['logout'])) {
     <script src="js/portal_transition.js"></script>
     <script src="js/dropdown.js"></script>
 </body>
+
 </html>
