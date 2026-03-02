6ed832912ac5e271338ee2ebfaf11bea11cb99ec
Sand Rex <137732075+giansandrexx@users.noreply.github.com>
2026-03-02 10:30:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -52,8 +52,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="sratool/css/index.css">
-    <link rel="stylesheet" href="sratool/css/responsive.css">
+    <link rel="stylesheet" href="./sratool/css/index.css">
+    <link rel="stylesheet" href="./sratool/css/responsive.css">
 </head>
 <body>
 
