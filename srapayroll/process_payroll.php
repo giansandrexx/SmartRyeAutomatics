c21ffc65a511fbe768f3cd9fadd8463ee0c70d03
Sand Rex <137732075+giansandrexx@users.noreply.github.com>
2026-03-04 12:21:16 +0800

    Updated

--- srapayroll/process_payroll.php
+++ srapayroll/process_payroll.php
@@ -109,7 +109,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SRA Payroll – Process Payroll</title>
+    <title>SRA Payroll</title>
     <link rel="icon" type="image/png" sizes="32x32" href="../sratool/img/favicon-32x32.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="css/process.css">
@@ -379,3 +379,4 @@ compute();
 </body>
 
 </html>
+
