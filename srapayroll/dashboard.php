5b558ac40c89b900d5e2ba1a20ba66f197970bff
Sand Rex <137732075+giansandrexx@users.noreply.github.com>
2026-03-05 01:07:44 +0800

    Updated

--- srapayroll/dashboard.php
+++ srapayroll/dashboard.php
@@ -38,7 +38,7 @@ $conn->close();
     <title>SRA Payroll</title>
     <link rel="icon" type="image/png" sizes="32x32" href="../sratool/img/favicon-32x32.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="/css/dashboard.css">
+    <link rel="stylesheet" href="css/dashboard.css">
 </head>
 <body>
 
@@ -183,3 +183,4 @@ $conn->close();
 </body>
 
 </html>
+
