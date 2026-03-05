01b6fa2051be0af3630ac54959cb006dc26b7fa5
Sand Rex <137732075+giansandrexx@users.noreply.github.com>
2026-03-05 11:00:54 +0800

    Updated

--- srapayroll/employees.php
+++ srapayroll/employees.php
@@ -27,8 +27,8 @@ function getInitials($name) {
     <title>SRA Payroll</title>
     <link rel="icon" type="image/png" sizes="32x32" href="../sratool/img/favicon-32x32.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="../srapayroll/css/employees.css">
-    <link rel="stylesheet" href="../srapayroll/css/emp-modal.css">
+    <link rel="stylesheet" href="css/employees.css">
+
 </head>
 <body>
 
@@ -183,4 +183,5 @@ function getInitials($name) {
 
 <script src="../srapayroll/js/employee.js"></script>
 </body>
+
 </html>
