0c6b958a60688b572d8302e49a1465c13f5721f8
Sand Rex <137732075+giansandrexx@users.noreply.github.com>
2026-03-11 11:44:54 +0800

    Updated

--- system_logs.php
+++ system_logs.php
@@ -66,7 +66,7 @@ $qstring = $qparams ? '&' . http_build_query($qparams) : '';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>System Logs — SRA Portal</title>
+    <title>System Logs</title>
     <link rel="icon" type="image/png" sizes="32x32" href="sratool/img/favicon-32x32.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -343,4 +343,5 @@ $qstring = $qparams ? '&' . http_build_query($qparams) : '';
 <script src="js/dropdown.js"></script>
 </body>
 </html>
+
 
